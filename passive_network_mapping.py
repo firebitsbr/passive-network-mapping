1d83ba96b623fdec0c1792d16f9c805296b34487
gdflsl <gaetan.deflesselle@tutanota.com>
2016-05-24 14:30:10 +0200

    Update passive_network_mapping.py

--- passive_network_mapping.py
+++ passive_network_mapping.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 '''Warning remover'''
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
@@ -7,23 +6,16 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
 
+adressList=[] 
 
-adressList=[] # Will contain the list of found IP addresses
-
-def list(adressList): 
-
-	def readPacket(pkt):
-
-		if pkt.haslayer(IP): # If our packet has an IP layer
-			IPadress = pkt[IP].src # get IP adress (located in IP layer)
-			MACadress = pkt[Ether].src # get MAC adress (located in Ether Layer)
-
-			if IPadress not in adressList: 	# Then If IP never found before
-				adressList.append(IPadress) # Add to our list
-				print ('[+] Host Found\nMAC: ' + MACadress + ' | IP: '+ IPadress + '\n') # Print caracteristics
-
-	return readPacket
+def readPacket(pkt):
+	if pkt.haslayer(IP): # If our packet has an IP layer
+		IPadress = pkt[IP].src # get IP adress (located in IP layer)
+		MACadress = pkt[Ether].src # get MAC adress (located in Ether Layer)
+		if IPadress not in adressList: 	# Then If IP never found before
+			adressList.append(IPadress) # Add to our list
+			print ('[+] Host Found\nMAC: ' + MACadress + ' | IP: '+ IPadress + '\n') # Print caracteristics
 
 
 print('[+] Listening... \n')
-sniff(prn=list(adressList)) # Sniff any type of packet, send each of them to readPacket()
+sniff(prn=readPacket)
